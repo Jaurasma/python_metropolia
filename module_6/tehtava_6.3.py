e740c16e4ec00085cea5807226307ea18332d961
Jaurasma <jaurasmaa@gmail.com>
2026-01-27 13:18:56 +0200

    changed ex 6.3 gallon to gloabl var

--- module_6/tehtava_6.3.py
+++ module_6/tehtava_6.3.py
@@ -1,8 +1,10 @@
 # Kirjoita funktio, joka saa parametrinaan bensiinin määrän Yhdysvaltain nestegallonoina ja palauttaa paluuarvonaan vastaavan litramäärän. Kirjoita pääohjelma, joka kysyy gallonamäärän käyttäjältä ja muuntaa sen litroiksi. Muunnos on tehtävä aliohjelmaa hyödyntäen. Muuntamista jatketaan siihen saakka, kunnes käyttäjä syöttää negatiivisen gallonamäärän.
 # Yksi gallona on 3,785 litraa.
 
+GALLON = 3.785
+
 def gallons_to_liters(gallons):
-    liters = gallons * 3.785
+    liters = gallons * GALLON
     return liters
 
 def main():
